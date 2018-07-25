c18c6bbe84ea60debdb8b90b757c6c1f3d4c010b
KatherineMurr <katherinemurr@yahoo.com>
2018-07-25 21:27:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,5 @@ var driver = {}
 function updateDriverWithKeyAndValue(driver,key, value) {
   return Object.assign({}, driver, {[key]: value});
 }
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+  
